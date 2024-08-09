1c61b9f63ed702d98ab5f92d701fc67383a86b0f
lakshminagireddy <lakshminagireddy.g@gmail.com>
2024-08-09 18:09:06 +0530

    New Scripts

---
+++ src/Locator2.java
@@ -0,0 +1,12 @@
+
+public class Locator2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+	public void getpwd() {
+		
+	}
+
+}
--- src/Locators.java
+++ src/Locators.java
@@ -1,22 +1,31 @@
-import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.NoSuchElementException;
 
 public class Locators {
 
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
-		//driver.manage().timeouts().implicitlyWait(5, null);
-		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		//String name = null;
+		//driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("nagi");
-		driver.findElement(By.name("inputPassword")).sendKeys("12345678");
+		driver.findElement(By.name("inputPassword")).sendKeys("123456");
 		driver.findElement(By.className("signInBtn")).click();
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
-		driver.findElement(By.linkText("Forgot your password?")).click();
-		driver.quit();
-
+ 		driver.findElement(By.linkText("Forgot your password?")).click();
+ 		driver.findElement(By.xpath("//input[@placeholder=\"Name\"]")).sendKeys("lakshmi");
+		driver.findElement(By.xpath("//input[@placeholder=\"Email\"]")).sendKeys("lakshmi.g@gmail.com");
+		driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("9848189751");
+		driver.findElement(By.xpath("//button[@class='reset-pwd-btn']")).click();
+		//System.out.println(driver.findElement(By.xpath("//p[@class='infoMsg']")).getText());
+		//String passwordarray=driver.findElement(By.xpath("//p[@class='infoMsg']")).getText();
+		//String[] passwordarray1=passwordarray.split("'");
+		//String[] passwordarray2=passwordarray1[1].split("'");
+		//System.out.println(passwordarray2);
+		
 	}
 }
